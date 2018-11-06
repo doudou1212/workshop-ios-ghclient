b1fc4eb527ed5b21628bc6fca64a8c63f78d31eb
Jing Sun <jing.sun@rea-group.com>
2018-11-06 12:20:10 +0800

    work shop 3: render table view

---
+++ GHClient/NewsUITableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  UITableViewCell.swift
+//  GHClient
+//
+//  Created by Jing Sun  on 2018/11/1.
+//  Copyright © 2018 com.thoughtworks.workshop. All rights reserved.
+//
+
+import UIKit
+
+class NewsUITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func configure(with event: Event) {
+        
+    }
+    
+}
--- GHClient/ViewController.swift
+++ GHClient/ViewController.swift
@@ -8,12 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as? NewsUITableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let event = FakeDataProvider().providerData()[indexPath.row]
+       cell.configure(with: event)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    private let reuseIdentifier = "NewsUITableViewCell"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "NewsUITableViewCell", bundle: nil), forCellReuseIdentifier: reuseIdentifier)
+    }
+    
+    
 }
 
